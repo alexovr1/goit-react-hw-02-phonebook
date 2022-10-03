5bbe58b464ac3292d88e55412c17b47038b7d500
Oleksandr Ovrakh <oleksandr.ovrakh@gmail.com>
2022-10-03 19:19:14 +0300

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,54 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { nanoid } from 'nanoid';
+
+import { Component } from 'react';
+
+import { Form } from './Form/Form';
+
+import { Contacts } from './Contacts/Contacts';
+
+export class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  handleSubmit = ({ name, number }, actions) => {
+    const normalizedName = name.toLowerCase();
+    if (
+      !this.state.contacts.find(
+        contact => contact.name.toLowerCase() === normalizedName
+      )
+    ) {
+      this.setState(prevstate => ({
+        contacts: [...prevstate.contacts, { id: nanoid(), name, number }],
+      }));
+    } else {
+      alert(`${name} is already is contacts`);
+    }
+    actions.resetForm();
+  };
+
+  handleDelete = name => {
+    this.setState(prevstate => ({
+      contacts: prevstate.contacts.filter(contact => contact.name !== name),
+    }));
+  };
+
+  render() {
+    const { contacts } = this.state;
+
+    return (
+      <div>
+        <h1>Phonebook</h1>
+        <Form handleSubmit={this.handleSubmit} />
+        <h2>Contacts</h2>
+        <Contacts contacts={contacts} handleDelete={this.handleDelete} />
+      </div>
+    );
+  }
+}
